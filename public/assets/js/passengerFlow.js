ad36443b990bae731e80e77493fb15f112829610
Huang Zhenyang <745125931@qq.com>
2017-04-30 17:03:42 +0800

    修改passengerFlow.html 和 passengerFlow.js

--- public/assets/js/passengerFlow.js
+++ public/assets/js/passengerFlow.js
@@ -3,7 +3,7 @@
 /*
 * 实时（每隔很短的时间向服务器请求数据）
 */
-var passengerFlowData = []; //用于存取数据 [['1997/07/06', 626],['1992/01/06',677]]
+//passengerFlowData : [] --> 用于存取数据 [['1997/07/06', 626],['1992/01/06',677]]
 
 /*
 $(function(){
@@ -18,7 +18,6 @@ var getPassengerFlowData = function(){
 		type:"get",
 		url:"passengerFlowData.do",
 		dataType:"json",
-		
 	}).done((data)=>{
 		//data: {time:'1997/05/07',value:753}
 		console.log('成功, 收到的数据: ' + JSON.stringify(data, null, '  '));
@@ -27,10 +26,8 @@ var getPassengerFlowData = function(){
 		tempList.push(result.time);
 		tempList.push(result.value);
 		passengerFlowData.push(tempList);
-		
 	}).fail((xhr, status)=>{
 		console.log('失败: ' + xhr.status + ', 原因: ' + status);
 	});
 	
-	return passengerFlowData;
 }
