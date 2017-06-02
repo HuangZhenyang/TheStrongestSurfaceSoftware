8b47b075cb61b74eca339103008aefeff61711ab
Huang Zhenyang <745125931@qq.com>
2017-06-02 13:13:26 +0800

    增加interface_doc.md内容

--- docs/interface_doc.md
+++ docs/interface_doc.md
@@ -269,12 +269,13 @@
 #### ** 修改探针状态 **
 1. Request URL:  `/config/setwifistatus.do`
 2. Request Method: `POST`
-3. ```json
+3. 
+```json
 	{
 		id:"024",
 		status:"off"
 	}
-   ```
+```
 4. Data Type: `JSON`
 5. Description：
 	1. id: `探针id`
@@ -315,12 +316,13 @@
 #### **设置深访跳出率阈值**
 1. Request URL: `/config/thresholddj.do`
 2. Request Method: `POST`
-3. ```json
+3. 
+```json
 	{
 		deep:5500,
 		jump:3600
 	} 
-   ```
+```
 4. Data Type: `JSON`
 5. Description:
 	1. deep: `深访率阈值`
@@ -357,3 +359,32 @@
 		1.	deep: `深访`
 		2.	jump: `跳出`
 6. Data From To: Server To Client
+
+---
+
+
+---
+### **comeperiod.js**
+1. Request URL: `/table/comeperiod.do`
+2. Request Method: `GET`
+3. 
+```json
+	{
+		result:[
+			{mac:"B8******AFA8", telbrand:"华为", "comeperiod":5, staytime:0.5, frequency:7, lasttime:"2016-09-07"},
+			{mac:"C7******ADV8", telbrand:"小米", "comeperiod":1, staytime:1, frequency:12, lasttime:"2016-09-07"}
+		]
+	} 
+```
+4. Data Type: `JSON`
+5. Description:
+		1. mac: `手机mac地址`
+		2. telbrand: `手机品牌`
+		3. comeperiod: `来访周期`
+		4. staytime: `驻店时长`
+		5. frequency: `到店次数`
+		6. lasttime: `最后一次到店时间`
+6. Data From To:  Server To Client
+
+
+
--- public/assets/js/passengerFlow.js
+++ public/assets/js/passengerFlow.js
@@ -1,21 +1,129 @@
 'use strict';
 
-$(document).ready(function(){
-	
+/*
+ * 变量
+ */
+var myChart = echarts.init(document.getElementById('realTimePassengerFlow'));
+ // 图表数据： [{value:["2/4",121]},{value:["3/4",22]}]
+var pfData = [];
+
+$(document).ready(function () {
+	initPFChart();
 });
 
-var myChart = echarts.init(document.getElementById('realTimePassengerFlow'));
+/*
+ * 实时客流图
+ */
+
+function initPFChart() {
+	/*
+	function randomData() {
+		now = new Date(+now + oneDay);
+		value = value + Math.random() * 21 - 10;
+		return {
+			//name: now.toString(),
+			value: [
+            	[now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+				Math.round(value)
+        	]
+		}
+
+	}
+
+
+	var now = +new Date(1997, 9, 3);
+	var oneDay = 24 * 3600 * 1000;
+	var value = Math.random() * 1000;
+	for (var i = 0; i < 10; i++) {
+		data.push(randomData());
+	}*/
+	
+	var option = {
+		title: {
+			text: '动态数据 + 时间坐标轴'
+		},
+		tooltip: {
+			trigger: 'axis',
+			formatter: function (params) {
+				params = params[0];
+				//var date = new Date(params.name);
+				//return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+				return params.value[0] + ':' + params.value[1];
+
+			},
+			axisPointer: {
+				animation: false
+			}
+		},
+		xAxis: {
+			type: 'time',
+			splitLine: {
+				show: false
+			}
+		},
+		yAxis: {
+			type: 'value',
+			boundaryGap: [0, '100%'],
+			splitLine: {
+				show: false
+			}
+		},
+		series: [{
+			name: '模拟数据',
+			type: 'line',
+			showSymbol: false,
+			hoverAnimation: false,
+			data: pfData
+    }]
+	};
+
+	myChart.setOption(option);
+
+	setInterval(function () {
+		$.ajax({
+			type: 'get',
+			url: '/graph/realTimePassengerflow.do',
+			dataType: 'json',
+		}).done(function (data) {
+			console.log('成功, 收到的数据: ' + JSON.stringify(data, null, '  '));
+			pfData.push(data);
+			myChart.setOption({
+				series: [{
+					data: pfData
+        		}]
+			});
+				
+			console.log(pfData);
+		}).fail(function (xhr, status) {
+			console.log('失败: ' + xhr.status + ', 原因: ' + status);
+		});
+
+
+	}, 5000);
+
+	
+}
+
+
+
+
+
+
+
+/*
+
 function randomData() {
-    now = new Date(+now + oneDay);
-    value = value + Math.random() * 21 - 10;
-    return {
-        //name: now.toString(),
-        value: [
+
+	now = new Date(+now + oneDay);
+	value = value + Math.random() * 21 - 10;
+	return {
+		//name: now.toString(),
+		value: [
             [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
 			Math.round(value)
         ]
-    }
-	
+	}
+
 }
 
 var data = [];
@@ -23,48 +131,48 @@ var now = +new Date(1997, 9, 3);
 var oneDay = 24 * 3600 * 1000;
 var value = Math.random() * 1000;
 for (var i = 0; i < 10; i++) {
-    data.push(randomData());
+	data.push(randomData());
 }
 
 var option = {
-    title: {
-        text: '动态数据 + 时间坐标轴'
-    },
-    tooltip: {
-        trigger: 'axis',
-        formatter: function (params) {
-			
-            params = params[0];
+	title: {
+		text: '动态数据 + 时间坐标轴'
+	},
+	tooltip: {
+		trigger: 'axis',
+		formatter: function (params) {
+
+			params = params[0];
 			//console.log("params value " + params.value);
 			//console.log("params value[0] " + params.value[0]);
 			//console.log("params value[1] " + params.value[1]);
-            var date = new Date(params.name);
-            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
-		
-        },
-        axisPointer: {
-            animation: false
-        }
-    },
-    xAxis: {
-        type: 'time',
-        splitLine: {
-            show: false
-        }
-    },
-    yAxis: {
-        type: 'value',
-        boundaryGap: [0, '100%'],
-        splitLine: {
-            show: false
-        }
-    },
-    series: [{
-        name: '模拟数据',
-        type: 'line',
-        showSymbol: false,
-        hoverAnimation: false,
-        data: data
+			var date = new Date(params.name);
+			return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+
+		},
+		axisPointer: {
+			animation: false
+		}
+	},
+	xAxis: {
+		type: 'time',
+		splitLine: {
+			show: false
+		}
+	},
+	yAxis: {
+		type: 'value',
+		boundaryGap: [0, '100%'],
+		splitLine: {
+			show: false
+		}
+	},
+	series: [{
+		name: '模拟数据',
+		type: 'line',
+		showSymbol: false,
+		hoverAnimation: false,
+		data: data
     }]
 };
 
@@ -72,17 +180,18 @@ myChart.setOption(option);
 
 setInterval(function () {
 
-    for (var i = 0; i < 5; i++) {
-        data.shift();
-        data.push(randomData());
-	
-    }
+	for (var i = 0; i < 5; i++) {
+		data.shift();
+		data.push(randomData());
 
-    myChart.setOption({
-        series: [{
-            data: data
+	}
+
+	myChart.setOption({
+		series: [{
+			data: data
         }]
-    });
+	});
 }, 10000);
 
 console.log(data);
+*/
